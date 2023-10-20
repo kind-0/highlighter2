1a0851edcab7123875de73c1ab7976eb7965a935
Pablo Fernandez <p@f7z.io>
2023-10-20 11:17:23 +0300

    add semicolons

--- apps/web/src/routes/p/[id]/+layout.ts
+++ apps/web/src/routes/p/[id]/+layout.ts
@@ -1,27 +1,27 @@
 import { NDKUser } from "@nostr-dev-kit/ndk";
 
-export async function load({ params }) {   
+export async function load({ params }) {
     const {id} = params;
 
     if (id.startsWith('npub')) {
         return { npub: id };
     }
 
-    let npub = ``
-   
+    let npub = ``;
+
     try {
         const user = await NDKUser.fromNip05(id);
         if(user) {
-            npub = user.npub
+            npub = user.npub;
         }
     } catch(e) {
         console.log(e, ` error`);
     }
 
     if(!npub) {
-        npub = id
+        npub = id;
     }
-    
+
     return {
         npub,
     };
