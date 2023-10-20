256f2fc0e9ac3cf006d8b837e514a2a1fc41dbb7
Pablo Fernandez <p@f7z.io>
2023-10-20 11:18:49 +0300

    comment out WIP share button @triesap

--- apps/web/src/lib/components/DrawerInnerContainer.svelte
+++ apps/web/src/lib/components/DrawerInnerContainer.svelte
@@ -13,18 +13,18 @@
         <button on:click={async () => await closeDrawerCallback()}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-            </svg> 
+            </svg>
         </button>
     </div>
     <div class="flex flex-col w-full justify-center items-center">
-        <ShareButton class="btn-sm" options={{
+        <!-- <ShareButton class="btn-sm" options={{
             title: shareTitle,
             text: shareText,
             url: shareUrl,
             dialogTitle: 'highlighter'
         }}>
             share
-        </ShareButton>
+        </ShareButton> -->
     </div>
     <slot />
 </div>
